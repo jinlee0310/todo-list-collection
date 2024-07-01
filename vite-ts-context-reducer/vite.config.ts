03cfbd5caae2efb9c70b14ababd51cfcf0440d41
jinlee0310 <onenmore.jin@gmail.com>
2024-07-01 20:10:37 +0900

    feat: 절대경로 설정

--- vite-ts-context-reducer/vite.config.ts
+++ vite-ts-context-reducer/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: [
+            { find: "@assets", replacement: "/src/assets" },
+            { find: "@components", replacement: "/src/components" },
+            { find: "@constants", replacement: "/src/constants" },
+            { find: "@containers", replacement: "/src/containers" },
+            { find: "@contexts", replacement: "/src/contexts" },
+            { find: "@hooks", replacement: "/src/hooks" },
+            { find: "@models", replacement: "/src/models" },
+            { find: "@reducers", replacement: "/src/reducers" },
+            { find: "@", replacement: "/src" },
+        ],
+    },
+});
