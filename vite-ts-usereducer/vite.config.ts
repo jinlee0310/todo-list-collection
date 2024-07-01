88f01e1634bc4b46576e6c350c07e9e58ef1ec33
jinlee0310 <onenmore.jin@gmail.com>
2024-07-01 16:10:30 +0900

    feat: 절대경로 설정

--- vite-ts-usereducer/vite.config.ts
+++ vite-ts-usereducer/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: [
+            { find: "@assets", replacement: "/src/assets" },
+            { find: "@components", replacement: "/src/components" },
+            { find: "@constants", replacement: "/src/constants" },
+            { find: "@containers", replacement: "/src/containers" },
+            { find: "@hooks", replacement: "/src/hooks" },
+            { find: "@models", replacement: "/src/models" },
+            { find: "@", replacement: "/src" },
+        ],
+    },
+});
