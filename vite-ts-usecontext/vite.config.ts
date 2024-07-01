7f7a78d80a81b0cc93cb94df9dae2d93eebfb4c3
jinlee0310 <onenmore.jin@gmail.com>
2024-07-01 13:45:04 +0900

    feat: 절대 경로 설정

--- vite-ts-usecontext/vite.config.ts
+++ vite-ts-usecontext/vite.config.ts
@@ -5,4 +5,16 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), svgr()],
+    resolve: {
+        alias: [
+            { find: "@assets", replacement: "/src/assets" },
+            { find: "@components", replacement: "/src/components" },
+            { find: "@constants", replacement: "/src/constants" },
+            { find: "@containers", replacement: "/src/containers" },
+            { find: "@contexts", replacement: "/src/contexts" },
+            { find: "@hooks", replacement: "/src/hooks" },
+            { find: "@models", replacement: "/src/models" },
+            { find: "@", replacement: "/src" },
+        ],
+    },
 });
