ce9684470c80bacc07ee76bac57601c92007c762
jinlee0310 <onenmore.jin@gmail.com>
2024-08-03 19:38:11 +0900

    feat: packages/ui 추가

---
+++ packages/ui/src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import "./style.css";
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>
+        to<span>do.</span>
+      </h1>
+    </header>
+  );
+}
