687cc0d0a48779f80e100b8de3801d5d3852db05
jinlee0310 <onenmore.jin@gmail.com>
2024-08-03 19:39:34 +0900

    feat: create-vite, without-cra 폴더를 app/ 아래로 이동

--- app/without-cra/src/App.jsx
+++ app/without-cra/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Header from "@todo-list-collection/ui/header";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, React</h1>
+      <Header />
     </div>
   );
 }
